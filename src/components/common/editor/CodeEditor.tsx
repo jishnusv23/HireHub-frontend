abd2e2fb2581686022cb5b8bb83b67d9a735021f
jishnuvadakara <jishnuv750@gmail.com>
2024-09-23 16:14:57 +0530

    feat(CodeEditor):add debounce for the

--- src/components/common/editor/CodeEditor.tsx
+++ src/components/common/editor/CodeEditor.tsx
@@ -1,20 +1,23 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import { Button } from "@/components/ui/button";
 import OutPut from "./OutPut";
 import LanguageSelector from "./LanguageSelector";
 import { SocketContext } from "@/context/SocketProvider";
-interface CodeEditorProps{
-  roomId:string
+import { debounce } from "lodash"; 
+interface CodeEditorProps {
+  roomId: string;
 }
-const CodeEditor:React.FC<CodeEditorProps> = ({roomId}) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ roomId }) => {
   const editorRef = useRef(null);
   const [language, setLanguage] = useState("javascript");
-  const [content, setContent] = useState('');
-  console.log("🚀 ~ file: CodeEditor.tsx:14 ~ content:", content)
+  const [content, setContent] = useState("");
+  console.log("🚀 ~ file: CodeEditor.tsx:14 ~ content:", content);
 
   const [showOutput, setShowOutput] = useState(false);
-  const {socket}=useContext(SocketContext)||{}
+  const { socket } = useContext(SocketContext) || {};
+    const isUpdatingRef = useRef(false);
+
 
   const onMount = (editor: any) => {
     editorRef.current = editor;
@@ -24,13 +27,39 @@ const CodeEditor:React.FC<CodeEditorProps> = ({roomId}) => {
   const handleRunClick = () => {
     setShowOutput(true);
   };
-  const handleEditorChange=(value:string|undefined)=>{
-    if(value!=undefined){
-      setContent(value)
-      socket?.emit("code-change", { roomId, value });
-    }
+  useEffect(() => {
+    if (socket) {
+      socket?.on("update-code", (newContent ) => {
+        console.log(newContent,'________________________________')
+        if (editorRef.current) {
+          isUpdatingRef.current = true;
+          const editor = editorRef.current as any;
+          const position = editor.getPosition();
+          editor.setValue(newContent);
+          editor.setPosition(position);
+          setContent(newContent);
+          isUpdatingRef.current = false;
+        }
+      });
 
-  }
+      return ()=>{
+        socket?.off('update-code')
+      }
+    }
+  }, [socket]);
+    const debouncedEmit = useRef(
+      debounce((content: string) => {
+        if (!isUpdatingRef.current) {
+          socket?.emit("code-change", { roomId, content });
+        }
+      }, 500)
+    ).current;
+  const handleEditorChange = (value: string | undefined) => {
+    if (value != undefined && !isUpdatingRef.current) {
+      setContent(value);
+      debouncedEmit(value)
+    }
+  };
 
   return (
     <div className="relative h-screen">
