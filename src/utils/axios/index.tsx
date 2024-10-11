83459e81265a4f5341316d87e47a697d5030dda0
jishnuvadakara <jishnuv750@gmail.com>
2024-10-11 14:11:10 +0530

    update:added host url

--- src/utils/axios/index.tsx
+++ src/utils/axios/index.tsx
@@ -1,18 +1,34 @@
-import axios, { AxiosError } from "axios";
-export const BASE_URL = "http://localhost:2001";
+// import axios, { AxiosError } from "axios";
+// export const BASE_URL = "http://localhost:2001";
+
+// export const CLIENT_API = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true,
+// });
+
+// CLIENT_API.interceptors.response.use(
+//   (response) => {
+    
+//     return response;
+//   },
+//   (error: AxiosError) => {
+//     console.log(error.message, "error interceptors");
+//     throw error;
+//   }
+// );
+
+import axios from "axios";
+
+const isDevelopment =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "4.188.113.7";
+
+export const BASE_URL = isDevelopment
+  ? "http://localhost:2001"
+  : "http://trend-vista.shop";
+
+console.log(BASE_URL, "hee");
 
 export const CLIENT_API = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
 });
-
-CLIENT_API.interceptors.response.use(
-  (response) => {
-    // console.log(response.data, "interceptor");
-    return response;
-  },
-  (error: AxiosError) => {
-    console.log(error.message, "error interceptors");
-    throw error;
-  }
-);
