e36d1398b0362101f3b1dde1c14401da97bd6f2a
jishnuvadakara <jishnuv750@gmail.com>
2024-10-11 15:35:46 +0530

    feat:update config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//     plugins: [react()],
+//     resolve: {
+//         alias: {
+//             "@": path.resolve(process.cwd(), "./src"),
+//             "@lib": path.resolve(process.cwd(), "./src/lib"),
+//             "vite-env": path.resolve(__dirname, "src", "vite-env.d.ts"),
+//         },
+//     },
+//     define: {
+//         global: {},
+//     },
+// });
+
 import path from "path";
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(process.cwd(), "./src"),
-            "@lib": path.resolve(process.cwd(), "./src/lib"),
-            "vite-env": path.resolve(__dirname, "src", "vite-env.d.ts"),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    define: {
-        global: {},
-    },
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,37 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { resolve } from "path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import { resolve } from "path";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(process.cwd(), "./src"),
+//       "@lib": path.resolve(process.cwd(), "./src/lib"),
+//       "vite-env": path.resolve(__dirname, "src", "vite-env.d.ts"),
+//     },
+//   },
+//   define: {
+//     global: {},
+//   },
+// });
+
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(process.cwd(), "./src"),
-      "@lib": path.resolve(process.cwd(), "./src/lib"),
-      "vite-env": path.resolve(__dirname, "src", "vite-env.d.ts"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  define: {
-    global: {},
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
   },
 });
